d379e0bb4899ade2280a4686a20ee22845d091a3
Dharmendra-06 <23f2000072@ds.study.iitm.ac.in>
2026-06-19 04:39:21 +0530

    Data Tranformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,12 +1,13 @@
 import os
 import sys
-from src.exception import CustomExecption
+from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -66,12 +67,16 @@ class DataIngestion:
             )
 
         except Exception as e:
-            raise CustomExecption(e, sys)
+            raise CustomException(e, sys)
 
 
 if __name__ == '__main__':
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
+
 
     print("Train Data Path:", train_data)
     print("Test Data Path:", test_data)
--- src/exception.py
+++ src/exception.py
@@ -11,7 +11,7 @@ def error_message_detail(error, error_detail: sys):
     return error_message
 
 
-class CustomExecption(Exception):
+class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
@@ -26,4 +26,4 @@ if __name__=="__main__":
         a=1/0
     except Exception as e:
         logging.info("Divide by Zero")
-        raise CustomExecption(e,sys)
+        raise CustomException(e,sys)
